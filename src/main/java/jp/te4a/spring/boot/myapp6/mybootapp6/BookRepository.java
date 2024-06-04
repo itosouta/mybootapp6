b4d4e93d6e2e170940c0755ede16382665c8aaba
itosouta <te210832@gmail.com>
2024-06-04 11:23:49 +0900

    課題6

--- src/main/java/jp/te4a/spring/boot/myapp6/mybootapp6/BookRepository.java
+++ src/main/java/jp/te4a/spring/boot/myapp6/mybootapp6/BookRepository.java
@@ -1,3 +1,11 @@
+package jp.te4a.spring.boot.myapp6.mybootapp6;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.springframework.stereotype.Repository;
+
 @Repository
 public class BookRepository {
  private final ConcurrentMap<Integer, BookBean> bookMap
--- src/main/java/jp/te4a/spring/boot/myapp6/mybootapp6/BookService.java
+++ src/main/java/jp/te4a/spring/boot/myapp6/mybootapp6/BookService.java
@@ -1,3 +1,9 @@
+package jp.te4a.spring.boot.myapp6.mybootapp6;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 @Service
 public class BookService {
 
